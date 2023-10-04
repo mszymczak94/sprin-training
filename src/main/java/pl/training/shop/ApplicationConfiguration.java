8373c9013e6936680ec886b4e0cc5fc99872d487
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2023-10-04 12:40:05 +0200

    update

--- src/main/java/pl/training/shop/ApplicationConfiguration.java
+++ src/main/java/pl/training/shop/ApplicationConfiguration.java
@@ -1,77 +1,11 @@
 package pl.training.shop;
 
-import com.zaxxer.hikari.HikariDataSource;
-import jakarta.persistence.EntityManagerFactory;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
-import java.util.Properties;
-
-import static java.lang.Integer.parseInt;
-
-@PropertySource("classpath:jdbc.properties")
-@EnableAspectJAutoProxy
-@EnableTransactionManagement
-@EnableJpaRepositories//(repositoryImplementationPostfix = "Impl")
-@EnableWebMvc
-@ComponentScan(basePackages = "pl.training")
 @Configuration
 public class ApplicationConfiguration implements WebMvcConfigurer {
-
-    @Bean
-    public DataSource dataSource(Environment environment) {
-        var dataSource = new HikariDataSource();
-        dataSource.setUsername(environment.getProperty("database.username"));
-        dataSource.setPassword(environment.getProperty("database.password"));
-        dataSource.setJdbcUrl(environment.getProperty("database.url"));
-        dataSource.setDriverClassName(environment.getProperty("database.driver"));
-        dataSource.setMaximumPoolSize(parseInt(environment.getProperty("database.pool-size")));
-        return dataSource;
-    }
-
-    @Bean
-    public PropertiesFactoryBean jpaProperties() {
-        var factoryBean = new PropertiesFactoryBean();
-        factoryBean.setLocation(new ClassPathResource("jpa.properties"));
-        return factoryBean;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Properties jpaProperties) {
-        var factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setPackagesToScan("pl.training.shop");
-        factoryBean.setJpaProperties(jpaProperties);
-        factoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        return factoryBean;
-    }
-
-    // bean with name transactionManager is default tx manager
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
-        var factoryBean = new LocalValidatorFactoryBean();
-        factoryBean.setValidationMessageSource(messageSource);
-        return factoryBean;
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
