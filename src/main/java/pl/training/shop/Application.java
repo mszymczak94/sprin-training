b76cacd8a647da7af4927fc4f87297849fa50355
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2023-10-06 09:24:43 +0200

    update

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -6,10 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // ./mvnw -Pnative spring-boot:build-image
 // -Dspring.profiles.active=dev lub env SPRING_PROFILES_ACTIVE=dev
 @SpringBootApplication
-public class Application {
+public class Application /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }*/
+
 }
+
